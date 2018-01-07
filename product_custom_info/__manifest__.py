efdd04418c8f8a3b9435b2f531899835bb5f20ab
Fanha Giang <fanha99@hotmail.com>
2018-01-07 19:00:50 +0700

    [10][MIG] product_custom_info

--- product_custom_info/__manifest__.py
+++ product_custom_info/__manifest__.py
@@ -2,13 +2,13 @@
 # © 2015 Antiun Ingeniería S.L. - Sergio Teruel
 # © 2015 Antiun Ingeniería S.L. - Carlos Dauden
 # © 2016 Jairo Llopis <jairo.llopis@tecnativa.com>
-# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     'name': "Product Custom Info",
     'summary': "Add custom field in products",
     'category': 'Customize',
-    'version': '9.0.1.0.0',
+    'version': '10.0.1.0.0',
     'depends': [
         'product',
         'base_custom_info',
@@ -20,6 +20,6 @@
     'author': "Tecnativa, "
               "Odoo Community Association (OCA)",
     'website': 'http://www.tecnativa.com',
-    'license': 'LGPL-3',
+    'license': 'AGPL-3',
     'installable': True,
 }
