854cb88ac3608593bde82d93193bfe4eb6d929ec
Tonow-c2c <thomas.nowicki@camptocamp.com>
2020-07-31 10:17:02 +0200

    [IMP][13.0] Add product_template_tags_code module

---
+++ product_template_tags_code/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 Camptocamp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Template Tags",
+    "summary": "This addon allow to add code on products tags",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Camptocamp SA, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/product-attribute",
+    "depends": ["product", "product_template_tags"],
+    "data": ["views/product_template_tag.xml"],
+    "post_init_hook": "post_init_hook",
+}
