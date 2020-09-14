ac10c16d79110779481e9d978c297fca861d0c19
Simone Orsi <simahawk@gmail.com>
2020-09-14 15:17:48 +0200

    Add product_barcode_required

---
+++ product_barcode_required/models/product.py
@@ -0,0 +1,57 @@
+# Copyright 2020 Camptocamp SA
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl)
+
+from odoo import _, api, exceptions, models
+
+
+class BarcodeRequiredMixin(models.AbstractModel):
+    _name = "product.barcode.required.mixin"
+    _description = "Barcode required mixin"
+
+    @api.onchange("default_code")
+    def _onchange_code(self):
+        for rec in self:
+            if rec._is_barcode_required_enabled() and rec._is_barcode_required():
+                rec.barcode = rec.default_code
+
+    def _is_barcode_required(self):
+        self.ensure_one()
+        return not self.barcode and self.default_code
+
+    @api.model
+    def _is_barcode_required_enabled(self):
+        return self.env.company.product_variant_barcode_required
+
+
+class ProductTemplate(models.Model):
+    _name = "product.template"
+    _inherit = ["product.template", "product.barcode.required.mixin"]
+
+
+class ProductProduct(models.Model):
+    _name = "product.product"
+    _inherit = ["product.product", "product.barcode.required.mixin"]
+
+    @api.model
+    def create(self, vals):
+        rec = super().create(vals)
+        rec._check_barcode_required()
+        return rec
+
+    def write(self, vals):
+        res = super().write(vals)
+        self._check_barcode_required()
+        return res
+
+    def _check_barcode_required(self):
+        for rec in self:
+            if rec._is_barcode_required_enabled() and rec._is_barcode_required():
+                raise exceptions.ValidationError(
+                    _("Product '{0.name}' has no barcode!").format(rec)
+                )
+
+    def fields_get(self, allfields=None, attributes=None):
+        res = super().fields_get(allfields=allfields, attributes=attributes)
+        if "barcode" in res and self._is_barcode_required_enabled():
+            res["barcode"]["required"] = True
+        return res
