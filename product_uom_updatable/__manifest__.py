865017506e4bfbebb72409009b046f3f51980a28
Anna Janiszewska <anna.janiszewska@camptocamp.com>
2020-11-02 13:59:47 +0100

    Add product_uom_updatable module

---
+++ product_uom_updatable/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2020 Acsone SA/NV
+# Copyright 2020 Camptocamp
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Alc Product Uom Updatable",
+    "summary": """
+        allows products uom to be modified after be used in a stock picking if \
+         the product uom is of the same category""",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Acsone SA/NV, Odoo Community Association (OCA)",
+    "website": "www.acsone.eu",
+    "depends": ["stock"],
+    "data": [],
+    "demo": [],
+}
