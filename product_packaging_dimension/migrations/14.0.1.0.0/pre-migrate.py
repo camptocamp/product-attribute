7b550e0e3354952d3068fc0a86398bc219316585
Iryna Vyshnevska <iryna.vyshnevska@camptocamp.com>
2021-12-10 16:49:02 +0200

    fixup! fixup! [FIX] restore initial type for fields fix migration step

--- product_packaging_dimension/migrations/14.0.1.0.0/pre-migrate.py
+++ product_packaging_dimension/migrations/14.0.1.0.0/pre-migrate.py
@@ -5,6 +5,9 @@ from odoo.tools.sql import column_exists, rename_column
 
 
 def migrate(cr, version):
+
     # Rename lngth into packaging_length
-    if column_exists(cr, "product_packaging", "lngth"):
+    if column_exists(cr, "product_packaging", "packaging_length"):
+        cr.execute("UPDATE product_packaging SET packaging_length = lngth")
+    elif column_exists(cr, "product_packaging", "lngth"):
         rename_column(cr, "product_packaging", "lngth", "packaging_length")
