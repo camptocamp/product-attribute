3490665e9b7af2ad1c209c5028d80198a56af0bb
Khôi (Kiên Kim) <khoikk@trobz.com>
2025-03-18 11:23:21 +0700

    [MOV] stock_packaging_calculator_packaging_level: renamed to product_packaging_calculator_packaging_level

--- product_packaging_calculator_packaging_level/__manifest__.py
+++ product_packaging_calculator_packaging_level/__manifest__.py
@@ -1,16 +1,16 @@
 # Copyright 2021 Camptocamp SA
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl)
 {
-    "name": "Stock packaging calculator packaging level",
+    "name": "Product packaging calculator packaging level",
     "summary": "Glue module for packaging level",
     "version": "18.0.1.0.0",
     "development_status": "Alpha",
     "category": "Warehouse Management",
-    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "website": "https://github.com/OCA/product-attribute",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "LGPL-3",
     "application": False,
     "installable": True,
     "auto_install": False,
-    "depends": ["stock_packaging_calculator", "product_packaging_level"],
+    "depends": ["product_packaging_calculator", "product_packaging_level"],
 }
